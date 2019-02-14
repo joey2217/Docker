933be49729f7495527a013ea9ecad3dcf7342ce7
chengfei <chengfei@iuitech.com>
2019-02-14 15:48:30 +0800

    up

---
+++ Ubuntu/html/precache-manifest.4e8572ba98360aea28e99506f8d93c7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "386bc9877679b5941103",
+    "url": "/static/js/main.386bc987.chunk.js"
+  },
+  {
+    "revision": "1f9fcbef0684acc589a4",
+    "url": "/static/js/2.1f9fcbef.chunk.js"
+  },
+  {
+    "revision": "386bc9877679b5941103",
+    "url": "/static/css/main.eea3d432.chunk.css"
+  },
+  {
+    "revision": "9c9bebaa1478e029347775c42dd47155",
+    "url": "/index.html"
+  }
+];
